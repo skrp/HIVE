ee2d5ca508a283b49f4c364f26f1d79ee9543f5a
Prince of Archives <rich.tijerina@gmail.com>
2017-05-19 14:24:38 -0600

    Update HOST.pl

--- HOST.pl
+++ HOST.pl
@@ -1,12 +1,40 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
 use Proc::Daemon; use POSIX qw(mkfifo);
-#####################################
+#######################################
 # HOST - daemon code father
+# SETUP ###############################
+my $work = '.';
+# FILES 
+my $BUG = 'BUG'; my $LOG = 'LOG'; my $pid = 'PID';
+my $SLEEP = 'SLEEP'; my $SUICIDE = 'SUICIDE';
+# DAEMONIZE ##########################
+my $daemon = Proc::Daemon->new(
+    work_dir     => $work,
+    child_STDOUT => +>>$LOG,
+    child_STDERR => +>>$BUG,
+    pid_file     => $pid,
+);
+$daemon->Init();
 # INIT ##############################
-my $LOG = 'LOG'; open(my $Lfh, '>>', $LOG) or die "cant open LOG\n";
 my $WORD = 'WORD'; mkfifo($WORD, 0770) or die "mkfifo WORD fail\n"; # code location
 my $POST = 'POST'; mkfifo($POST, 0770) or die "mkfifo POST fail\n"; # wrapped code location
 while(1)
 {
-  
+  my $code = <$WORD>; chomp $code;
+  if (not defined $code)
+    { sleep 500; next; }
+  my @set = split(/\s+/, $code);
+  my $set_name = $set[0]; my $version = $set[1]; my $custom = $set[2]; my $tail = $set[3];
+  print "wrapped $set_name\n";
+}
+# FN ################################
+sub TIME
+{
+  my $t = localtime;
+  my $mon = (split(/\s+/, $t))[1];
+  my $day = (split(/\s+/, $t))[2];
+  my $hour = (split(/\s+/, $t))[3];
+  my $time = $mon.'_'.$day.'_'.$hour;
+  return $time;
+}
