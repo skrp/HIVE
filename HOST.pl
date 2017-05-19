09b9adc15274b0d6ec7ccc70018bdf75a26659e2
Prince of Archives <rich.tijerina@gmail.com>
2017-05-19 14:14:38 -0600

    Update HOST.pl

--- HOST.pl
+++ HOST.pl
@@ -4,3 +4,9 @@ use Proc::Daemon; use POSIX qw(mkfifo);
 #####################################
 # HOST - daemon code father
 # INIT ##############################
+my $LOG = 'LOG'; open(my $Lfh, '>>', $LOG) or die "cant open LOG\n";
+my $WORD = 'WORD'; mkfifo($WORD, 0770) or die "mkfifo WORD fail\n"; # code location
+my $POST = 'POST'; mkfifo($POST, 0770) or die "mkfifo POST fail\n"; # wrapped code location
+while(1)
+{
+  
