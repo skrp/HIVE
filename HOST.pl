f56645d8780856d600dd462b868b6d543890e15b
Prince of Archives <rich.tijerina@gmail.com>
2017-05-19 14:09:10 -0600

    Create HOST.pl

---
+++ HOST.pl
@@ -0,0 +1,6 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+use Proc::Daemon; use POSIX qw(mkfifo);
+#####################################
+# HOST - daemon code father
+# INIT ##############################
