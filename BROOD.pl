baabd2435a9749e02409c3981b5e454d1707230d
Prince of Archives <rich.tijerina@gmail.com>
2017-05-25 13:40:42 -0600

    Update BROOD.pl

--- BROOD.pl
+++ BROOD.pl
@@ -4,35 +4,42 @@ use Proc::Daemon; use POSIX qw(mkfifo);
 #######################################
 # BROOD - daemon birth mother
 # SETUP ###############################
+my $dir = ''; chdir $dir; #??????????????????????
 my $work = '.';
-# FILES 
+# FILES
 my $BUG = 'BUG'; my $LOG = 'LOG'; my $pid = 'PID';
 my $SLEEP = 'SLEEP'; my $SUICIDE = 'SUICIDE';
 # DAEMONIZE ##########################
 my $daemon = Proc::Daemon->new(
     work_dir     => $work,
-    child_STDOUT => +>>$LOG,
-    child_STDERR => +>>$BUG,
+    child_STDOUT => "+>>$LOG",
+    child_STDERR => "+>>$BUG",
     pid_file     => $pid,
 );
 $daemon->Init();
 # INIT ###############################
-my $WORD = 'WORD'; mkfifo($WORD, 0770) or die "mkfifo WORD fail\n"; # wrapped code location
-open($FWfh, '<', $WORD) or die "cant open WORD\n";
+my $WORD = 'WORD'; mkfifo($WORD, 0770) or die "mkfifo WORD fail\n"; # wrapped 
+
+code location
+open(my $FWfh, '<', $WORD) or die "cant open WORD\n";
 my $POST = 'POST'; mkfifo($POST, 0770) or die "mkfifo POST fail\n"; # $btime
-open($FPfh, '<', $POST) or die "cant open POST\n";
+open(my $FPfh, '<', $POST) or die "cant open POST\n";
 while(1)
 {
   my $code = <$WORD>; chomp $code;
   if (not defined $code)
-    { sleep 500; next; }
+    { sleep 60; next; }
   my $set_name = gen(); regen($set_name);
-# DIR 
-  my $home = "hive/$set_name"; my $dump = "$home/dump"; my $que = "$home/que";
+# DIR
+  my $home = "hive/$set_name"; my $dump = "$home/dump"; my $que = 
+
+"$home/que";
 # FILES
-  my $BUG = "BUG"; my $LOG = "LOG"; my $mPID = 'PID'; 
+  my $BUG = "BUG"; my $LOG = "LOG"; my $mPID = 'PID';
 # PREP ################################
-  mkdir $home or die "home FAIL\n"; mkdir $que or die "que FAIL\n"; mkdir $dump or die "dump FAIL\n";
+  mkdir $home or die "home FAIL\n"; mkdir $que or die "que FAIL\n"; mkdir 
+
+$dump or die "dump FAIL\n";
 # BIRTH ###############################
   my $embryo = Proc::Daemon->new(
     work_dir => $home,
@@ -42,20 +49,20 @@ while(1)
     exec_command => "perl $code",
   );
   $embryo->Init() or die "STILLBORN\n";
-  my $btime = TIME(); print $FPfh "$name $btime\n";
+  my $btime = TIME(); print $FPfh "$set_name $btime\n";
 }
 # FN ################################
 sub gen
 {
   open(my $Gfh, '<', 'GENERATION') or die "GENERATION fail\n";
-  my $set_name = readline $Gfh; close $Gfh; 
+  my $set_name = readline $Gfh; close $Gfh;
   return $set_name;
 }
 sub regen
 {
   my ($set_name) = shift; $set_name++;
   open(my $Gfh, '>', 'GENERATION') or die "REGENERATION fail\n";
-  print $Gfh "$set_name"; close $Gfh; 
+  print $Gfh "$set_name"; close $Gfh;
 }
 sub TIME
 {
