6b4e355d0417371ac0aa926839d27a0a12e96516
Prince of Archives <rich.tijerina@gmail.com>
2017-05-25 13:30:50 -0600

    Update HOST.pl

--- HOST.pl
+++ HOST.pl
@@ -4,24 +4,26 @@ use Proc::Daemon; use POSIX qw(mkfifo);
 #######################################
 # HOST - daemon code father
 # SETUP ###############################
+my $dir = '3'; chdir $dir;
 my $work = '.'; my $batch = 'batch';
-my $ver = './ver/'; my $dump ='./dump/';
-# FILES 
+my $ver = './ver/'; mkdir $ver;
+my $dump ='./dump/'; mkdir $dump;
+# FILES
 my $BUG = 'BUG'; my $LOG = 'LOG'; my $pid = 'PID';
 my $SLEEP = 'SLEEP'; my $SUICIDE = 'SUICIDE';
 # DAEMONIZE ##########################
 my $daemon = Proc::Daemon->new(
     work_dir     => $work,
-    child_STDOUT => +>>$LOG,
-    child_STDERR => +>>$BUG,
+    child_STDOUT => $LOG,
+    child_STDERR => $BUG,
     pid_file     => $pid,
 );
 $daemon->Init();
 # INIT ##############################
 my $WORD = 'WORD'; mkfifo($WORD, 0770) or die "mkfifo WORD fail\n"; # code location
-open($FWfh, '<', $WORD) or die "cant open WORD\n";
+open(my $FWfh, '<', $WORD) or die "cant open WORD\n";
 my $POST = 'POST'; mkfifo($POST, 0770) or die "mkfifo POST fail\n"; # wrapped code location
-open($FPfh, '<', $POST) or die "cant open POST\n";
+open(my $FPfh, '>', $POST) or die "cant open POST\n";
 while(1)
 {
   my $code = <$WORD>; chomp $code;
@@ -29,10 +31,10 @@ while(1)
     { sleep 500; next; }
   my @set = split(/\s+/, $code);
   my $set_name = $set[0]; my $version = $set[1]; my $custom = $set[2];
-  my $version =~ $ver.$version; my $wrapped = $dump.$set_name; 
-  my $tail = $version.'_tail'; my $custom =~ './batch/'.$custom;
+  $version =~ $ver.$version; my $wrapped = $dump.$set_name;
+  my $tail = $version.'_tail'; $custom =~ './batch/'.$custom;
   open(my $Wfh, '>>', $wrapped) or print "fail create $wrapped\n";
-  `cat $version >> $wrapped`; print $Wfh "\n"; 
+  `cat $version >> $wrapped`; print $Wfh "\n";
   `cat $custom >> $wrapped`; print $Wfh "\n";
   `cat $tail >> $wrapped`;
   print $FPfh "wrapped $set_name\n";
