1a7871f730a7b35d9f56dee368f5af05beb4445e
Prince of Archives <rich.tijerina@gmail.com>
2017-07-01 01:31:03 -0700

    Update HOST.pl

--- HOST.pl
+++ HOST.pl
@@ -4,13 +4,11 @@ use Proc::Daemon; use POSIX qw(mkfifo);
 #######################################
 # HOST - daemon code father
 # SETUP ###############################
-my $dir = ''; chdir $dir; #???????????????????
-my $work = '.'; my $batch = 'batch';
-my $ver = './ver/'; mkdir $ver;
-my $dump ='./dump/'; mkdir $dump;
+my $dir = '/tmp'; chdir $dir; 
+my $work = '.'; 
 # FILES
-my $BUG = 'BUG'; my $LOG = 'LOG'; my $pid = 'PID';
-my $SLEEP = 'SLEEP'; my $SUICIDE = 'SUICIDE';
+my $BUG = 'host_BUG'; my $LOG = 'host_LOG'; my $pid = 'host_PID';
+my $SLEEP = 'host_SLEEP'; my $SUICIDE = 'host_SUICIDE';
 # DAEMONIZE ##########################
 my $daemon = Proc::Daemon->new(
     work_dir     => $work,
@@ -20,24 +18,17 @@ my $daemon = Proc::Daemon->new(
 );
 $daemon->Init();
 # INIT ##############################
-my $WORD = 'WORD'; mkfifo($WORD, 0770) or die "mkfifo WORD fail\n"; # code location
-open(my $FWfh, '<', $WORD) or die "cant open WORD\n";
-my $POST = 'POST'; mkfifo($POST, 0770) or die "mkfifo POST fail\n"; # wrapped code location
-open(my $FPfh, '>', $POST) or die "cant open POST\n";
+my $WORD = 'RHOST'; mkfifo($WORD, 0770) or die "mkfifo WORD fail\n"; # code location
+open(my $FWfh, '<', $WORD) or die "cant open RHOST\n";
+my $POST = 'WHOST'; mkfifo($POST, 0770) or die "mkfifo POST fail\n"; # wrapped code location
+open(my $FPfh, '>', $POST) or die "cant open WHOST\n";
 while(1)
 {
   my $code = <$WORD>; chomp $code;
   if (not defined $code)
     { sleep 500; next; }
   my @set = split(/\s+/, $code);
-  my $set_name = $set[0]; my $version = $set[1]; my $custom = $set[2];
-  $version =~ $ver.$version; my $wrapped = $dump.$set_name;
-  my $tail = $version.'_tail'; $custom =~ './batch/'.$custom;
-  open(my $Wfh, '>>', $wrapped) or print "fail create $wrapped\n";
-  `cat $version >> $wrapped`; print $Wfh "\n";
-  `cat $custom >> $wrapped`; print $Wfh "\n";
-  `cat $tail >> $wrapped`;
-  print $FPfh "wrapped $set_name\n";
+  # api 
 }
 # FN ################################
 sub TIME
@@ -49,3 +40,4 @@ sub TIME
   my $time = $mon.'_'.$day.'_'.$hour;
   return $time;
 }
+# sanitize udp api req
