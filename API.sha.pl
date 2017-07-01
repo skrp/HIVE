8b85f9dc9cd0e58422e2b2ca35579d15991fdb7a
Prince of Archives <rich.tijerina@gmail.com>
2017-07-01 00:06:11 -0700

    Update API.sha.pl

--- API.sha.pl
+++ API.sha.pl
@@ -1,1 +1,24 @@
 #!/usr/local/bin/perl
+########################
+# API.sha - verify file 
+use strict; use warnings;
+use Digest::SHA ();
+# INPUT ###############
+my ($que) = @ARGV;
+open(my $qfh, '<', $que);
+my @que = readline $qfh; chomp @que;
+
+foreach my $file (@que)
+{
+    my ($sha) = file_digest($file) or die "couldn't sha $file";
+    if ($sha != $file)
+      { print $lfh "ERK! $file != $sha\n"; }
+}
+########################
+# SHA FN
+sub file_digest {
+    my ($filename) = @_;
+    my $digest = Digest::SHA->new(256);
+    $digest->addfile($filename, "b");
+    return $digest->hexdigest();
+}
