b7881181adf56433201e1c31b6e3be4925e70e4b
Prince of Archives <rich.tijerina@gmail.com>
2017-07-01 00:11:28 -0700

    Update API.sha.pl

--- API.sha.pl
+++ API.sha.pl
@@ -8,9 +8,7 @@ use Digest::SHA ();
 my ($que) = @ARGV;
 my @FACE; my $RATE = '100';
 # BIRTH ###############################
-my $embryo = Proc::Daemon->new(
-  work_dir => "/tmp/",
-);
+my $embryo = Proc::Daemon->new(work_dir => "/tmp/");
 my $pid = $embryo->Init() or die "STILLBORN\n";
 # LOCATION
 my $gravestone = "/graveyard/"$name"."tar";
