0874d43145f73aba0c255c1e70df6ddfed469641
Prince of Archives <rich.tijerina@gmail.com>
2017-07-02 05:34:43 -0700

    Update DEMON.pl

--- DEMON.pl
+++ DEMON.pl
@@ -29,28 +29,28 @@ open(my $Lfh, '>>', $log);
 # WORK ################################################
 while (1)
 {
-  open(my $qfh, '<', $que) or die "cant open que\n";
-  my @QUE = readline $qfh; chomp @QUE;
-  close $qfh;
-  my $stime = TIME(); print $Lfh "start $stime\n";
-  my $api = shift @QUE; print $Lfh "set $set_name\n";
-  my @apis = { "sha", "blockr", "xtrac", "get" };
-  die "bad api $api" unless grep(/$api/, @apis);
-  my $count = @QUE; print $Lfh "count $count\n"; my $ttl = $count;
-  foreach my $i (@QUE)
-  {
-  	if (-e $SUICIDE)
-    		{ SUICIDE($Lfh); }
-      	if (-e $SLEEP)
-    		{ SLEEP($Lfh); }
-     	print $Lfh "started $i\n";
-	switch ($api)
+	open(my $qfh, '<', $que) or die "cant open que\n";
+	my @QUE = readline $qfh; chomp @QUE;
+	close $qfh;
+	my $stime = TIME(); print $Lfh "start $stime\n";
+	my $api = shift @QUE; print $Lfh "set $set_name\n";
+	my @apis = { "sha", "blkr", "xtrac", "get" };
+	die "bad api $api" unless grep(/$api/, @apis);
+	my $count = @QUE; print $Lfh "count $count\n"; my $ttl = $count;
+	foreach my $i (@QUE)
 	{
-		case "sha" { sha($i) }
-		case "blockr" { blockr($i, $path) }
-		case "slicr" { slicr($i, $path) }
-		case "xtrac" { xtrac($i, $path) }
-		case "get" { eval{ get($i) } }
+		if (-e $SUICIDE)
+    			{ SUICIDE($Lfh); }
+      		if (-e $SLEEP)
+    			{ SLEEP($Lfh); }
+     		print $Lfh "started $i\n";
+		switch ($api)
+		{
+			case "sha" { sha($i) }
+			case "blkr" { blkr($i, $path) }
+			case "slicr" { slicr($i, $path) }
+			case "xtrac" { xtrac($i, $path) }
+			case "get" { eval{ get($i) } }
 	}
 ## CLEAN #############################################
 	shift @QUE; $count--;
@@ -66,63 +66,62 @@ while (1)
 # API ##################################################
 sub sha
 {
-  my $i = shift;
-  my ($sha) = file_digest($i);
-  if ($sha ne $i)
-    { print $Lfh "ERK! $file ne $sha\n"; }
-  print $Lfh "YAY $i\n";
+	my $i = shift;
+	my ($sha) = file_digest($i);
+	if ($sha ne $i)
+		{ print $Lfh "ERK! $file ne $sha\n"; }
+	print $Lfh "YAY $i\n";
 }
-sub blockr
+sub blkr
 {
-  my ($i, $path) = shift;
-  my $size = 10000;
-  my $st = stat($i);
-  my $total = $st->size;
-  my $count = $total / $size;
-  open(my $ifh, '<', "$i") || die "Cant open $i: $!\n";
-  binmode($ifh);
-  my $block = 0;
-  while (read ($ifh, $block, $size) <= $count)
-  {
-  	my $fh = new_block($path, $block);
-	print $fh $block;
-	close $fh;
-	$count += $size; 
-  }
-  print $Lfh "YAY $i\n";
+	my ($i, $path) = shift;
+	my $size = 10000;
+	my $st = stat($i);
+	my $total = $st->size;
+	my $count = $total / $size;
+	open(my $ifh, '<', "$i") || die "Cant open $i: $!\n";
+	binmode($ifh);
+	my $block = 0;
+	while (read ($ifh, $block, $size) <= $count)
+	{
+		my $fh = new_block($path, $block);
+		print $fh $block;
+		close $fh;
+		$count += $size; 
+	}
+	print $Lfh "YAY $i\n";
 }
 sub slicr
 {
-  my ($i, $path) = shift;
-
-  my $st = stat($i);
-  my $total = $st->size;
-  open(my $ifh, '<', "$i") || die "Cant open $i: $!\n";
-  binmode($ifh);
-  my $block = 0; my $position = 0;
-  while ($position < $total)
-  {
-    my $size = int(rand(99999));
-    if ($position + $size >= $total)
-      { $size = $total - $position; }
-    read($ifh, $block, $size);
-    my $fh = new_block($path, $block);
-    print $fh $block;
-    close $fh;
-    $count += $size; 
-  }
-  print $Lfh "YAY $i\n";
+	my ($i, $path) = shift;
+	my $st = stat($i);
+	my $total = $st->size;
+	open(my $ifh, '<', "$i") || die "Cant open $i: $!\n";
+	binmode($ifh);
+	my $block = 0; my $position = 0;
+	while ($position < $total)
+	{
+		my $size = int(rand(99999));
+		if ($position + $size >= $total)
+			{ $size = $total - $position; }
+		read($ifh, $block, $size);
+		my $fh = new_block($path, $block);
+		print $fh $block;
+		close $fh;
+		$count += $size; 
+	}
+	print $Lfh "YAY $i\n";
 }
 sub xtrac
 {
- my ($i, $path) = shift;
- my $archive = Archive::Tar->new($i);
- if ($archive->is_naughty)
- 	{ print $Lfh "ALERT xtrac naughty $i"; next; }
- my @files = $archive->files; print $Lfh @files;
- $archive->extract($dump);
- XS($dump, $path);
- print $Lfh "YAY $i\n";
+	my ($i, $path) = shift;
+	my $archive = Archive::Tar->new($i);
+	if ($archive->is_naughty)
+		{ print $Lfh "ALERT xtrac naughty $i"; next; }
+	my @files = $archive->files; print $Lfh @files;
+	$archive->extract($dump);
+	XS($dump, $path);
+	print $Lfh "YAY $i\n";
 }
 sub get
 {
@@ -153,46 +152,46 @@ sub tombstone
 }
 sub SUICIDE
 {
-  my $Lfh = shift;
-  unlink $SUICIDE;
-  my $xtime = TIME(); print $Lfh "FKTHEWORLD $xtime\n";
-  exit;
+	my $Lfh = shift;
+	unlink $SUICIDE;
+	my $xtime = TIME(); print $Lfh "FKTHEWORLD $xtime\n";
+	exit;
 }
 sub SLEEP
 {
-  my $Lfh = shift;
-  open(my $Sfh, '<', $SLEEP);
-  my $timeout = readline $Sfh; chomp $timeout;
-  my $ztime = TIME(); print $Lfh "sleep $ztime $timeout\n";
-  close $Sfh; unlink $SLEEP;
-  sleep $timeout;
+	my $Lfh = shift;
+	open(my $Sfh, '<', $SLEEP);
+	my $timeout = readline $Sfh; chomp $timeout;
+	my $ztime = TIME(); print $Lfh "sleep $ztime $timeout\n";
+	close $Sfh; unlink $SLEEP;
+	sleep $timeout;
 }
 sub TIME
 {
-  my $t = localtime;
-  my $mon = (split(/\s+/, $t))[1];
-  my $day = (split(/\s+/, $t))[2];
-  my $hour = (split(/\s+/, $t))[3];
-  my $time = $mon.'_'.$day.'_'.$hour;
-  return $time;
+	my $t = localtime;
+	my $mon = (split(/\s+/, $t))[1];
+	my $day = (split(/\s+/, $t))[2];
+	my $hour = (split(/\s+/, $t))[3];
+	my $time = $mon.'_'.$day.'_'.$hour;
+	return $time;
 }
 sub name
 {
-  my ($pid) = shift;
-  my @set = ("A".."Z", "a".."z", "1".."9");
-  my $id = $chars[rand @chars] for 1..8;
-  my $name = $name.$id
-  return $name;
+	my ($pid) = shift;
+	my @set = ("A".."Z", "a".."z", "1".."9");
+	my $id = $chars[rand @chars] for 1..8;
+	my $name = $name.$id;
+	return $name;
 }
 sub face
 { # FACE (age, name, rep, status)
-      my @FACE; 
-      my $wfifo = shift;
-      my $current = gmtime();
-      $FACE[0] = $name;
-      $FACE[1] = (($current - $born) / 60);
-      $FACE[3] = $set_name . '_' . $count . '/' . $ttl;
-      print $wfifo "@FACE";
+	my @FACE; 
+	my $wfifo = shift;
+	my $current = gmtime();
+	$FACE[0] = $name;
+	$FACE[1] = (($current - $born) / 60);
+	$FACE[3] = $set_name . '_' . $count . '/' . $ttl;
+	print $wfifo "@FACE";
 }
 sub XS
 {
