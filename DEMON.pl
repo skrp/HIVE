4e06e7c46f5f757fb325e341db42b0d046c6cd65
Prince of Archives <rich.tijerina@gmail.com>
2017-07-02 05:00:48 -0700

    Update DEMON.pl

--- DEMON.pl
+++ DEMON.pl
@@ -4,7 +4,9 @@ use Proc::Daemon;
 use File::Path;
 use Archive::Any ();
 use Digest::SHA ();
-use File::stat;
+use File::Copy;
+use File::LibMagic;
+use File::Find::Rule;
 #####################################
 # DEMON - summon scroll shell 
 # INIT ##############################
@@ -128,6 +130,64 @@ sub face
       $FACE[3] = $set_name . '_' . $count . '/' . $ttl;
       print $wfifo "@FACE";
 }
+sub XS
+{
+	my ($target, $dump) = shift;
+	if (not defined $target) { die "usage: TARGET ARGV[0] & dump argv[1]"; }
+	if (not defined $dump) { die "usage: target argv[0] & DUMP ARGV[1]"; }
+	my $rule = File::Find::Rule->file()->start($target);
+	my $magic = File::LibMagic->new();
+	while (defined( my $file = $rule->match) ) {
+		my ($sha) = file_digest($file) or die "couldn't sha $file";
+		File::Copy::copy($file, "$dump/pool/$sha");
+		my $cur = "$dump/g/g$sha";
+		open(my $fh, '>>', $cur) or die "Meta File Creation FAIL $file";
+		printf $fh "%s\n%s\n%s\n%s\n", 
+			xsname($file),
+			xspath($file),
+			xssize($file),
+			file_mime_encoding($file);
+}
+sub file_digest {
+	my ($filename) = @_;
+	my $digester = Digest::SHA->new('sha256');
+	$digester->addfile( $filename, 'b' );
+	return $digester->hexdigest;
+}
+sub xsname {
+	my ($filename) = @_;
+	$filename =~ s#^.*/##;
+	return $filename;
+}
+sub xspath {
+	my ($filename) = @_;
+	$filename =~ s#/#_#g;
+	return $filename; 
+}
+sub file_mime_encoding {
+	my ($filename) = @_;
+	my $info = $magic->info_from_filename($filename);
+	my $des = $info->{description};
+	$des =~ s#[/ ]#.#g;
+	$des =~ s/,/_/g;
+	my $md = $info->{mime_type};
+	$md =~ s#[/ ]#.#g;
+	my $enc = sprintf("%s %s %s", $des, $md, $info->{encoding}); 
+	return $enc;
+}
+sub xssize {
+	my $size = [ stat $_[0] ]->[7];
+	return $size;
+}
+sub new_block
+{
+	my ($path, $block) = shift;
+	my $sha = sha256_hex($block);
+	my $name = $path . $sha;
+	open(my $fh, '>', "$name") or die "Cant open $name: $!\n";
+	binmode($fh);
+	return *$fh;
+}
 # API ##################################################
 sub sha
 {
@@ -188,20 +248,4 @@ sub xtrac
  $archive->extract($dump);
  `XS $dump $path`;
  print $Lfh "YAY $i\n";
-}
-sub new_block
-{
-	my ($path, $block) = shift;
-	my $sha = sha256_hex($block);
-	my $name = $path . $sha;
-	open(my $fh, '>', "$name") or die "Cant open $name: $!\n";
-	binmode($fh);
-	return *$fh;
-}
-sub file_digest 
-{ 
-  my ($filename) = @_;
-  my $digest = Digest::SHA->new(256);
-  $digest->addfile($filename, "b");
-  return $digest->hexdigest();
 }
