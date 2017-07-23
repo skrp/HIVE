3291b5791804142c326af2ac6a551d81d13e3220
Prince of Archives <rich.tijerina@gmail.com>
2017-07-22 19:54:45 -0700

    Update DEMON.pl

--- DEMON.pl
+++ DEMON.pl
@@ -21,19 +21,19 @@ daemon() or die "FAIL daemon\n";
 # DIRS ###############################################
 # sea/ : blkr()
 # key/ : key()
-# cemetery/ : tombstone()
+# cemetery/ : $TOMBstone()
 # g/ : XS()
 # pool/ : XS()
 
 # GLOBAL CONST #######################################
 const my $PATH => $path;
-const my $NAME => name();
+const my $NAME => $NAME();
 const my $BIRTH => gmtime();
-const my $QUE => NAME.'_que';
-const my $DUMP => NAME.'_dump/';
-const my $TOMB => $PATH.'cemetery/'.NAME;
-const my $SLEEP => NAME.'_SLEEP';
-const my $SUICIDE => NAME.'_SUICIDE';
+const my $QUE => $NAME.'_que';
+const my $DUMP => $NAME.'_dump/';
+const my $$TOMB => $PATH.'cemetery/'.$NAME;
+const my $SLEEP => $NAME.'_SLEEP';
+const my $SUICIDE => $NAME.'_SUICIDE';
 const my $SIZE => 128000;
 const my $RATE => 100;
 const my @API => 'pop', 'chkmeta', 'index', 'blkr', 'build', 'vsha', 'xtrac', 'rgex', 'get';
@@ -43,34 +43,34 @@ my $YAY = 0;
 # PREP ###############################################
 chdir('/tmp/');
 
-mkdir DUMP or die "dump FAIL\n";
-open(my $Lfh, '>>', TOMB);
+mkdir $DUMP or die "$DUMP FAIL\n";
+open(my $Lfh, '>>', $TOMB);
 $Lfh->autoflush(1);
 
 printf $Lfh ("HELLOWORLD %s\n", TIME());
 while (1)
 { # WORK ################################################
-	unless (-e QUE)
-		{ sleep 3600; next; }
-	open(my $qfh, '<', QUE) or die "cant open QUE\n";
-	my @QUE = readline $qfh; chomp @QUE;
+	unless (-e $QUE)
+		{ $SLEEP 3600; next; }
+	open(my $qfh, '<', $QUE) or die "cant open $QUE\n";
+	my @$QUE = readline $qfh; chomp @$QUE;
 
-	my $api = shift @QUE; 
+	my $api = shift @$QUE; 
 	next if (api($api) < 0);
 	
-	my $ttl = @QUE; 
+	my $ttl = @$QUE; 
 	my $count = 0;
 	
-	foreach my $i (@QUE)
+	foreach my $i (@$QUE)
 	{
-		if (-e SUICIDE)
-    			{ SUICIDE(); }
-		if (-e SLEEP)
-   	 		{ SLEEP(); }
+		if (-e $SUICIDE)
+    			{ $SUICIDE(); }
+		if (-e $SLEEP)
+   	 		{ $SLEEP(); }
 		\&$api($i);
 		print $Lfh "started $i\n";
 		$count++;
-		tombstone($api, $count, $ttl) if ($count % 100 == 0);
+		$TOMBstone($api, $count, $ttl) if ($count % $RATE == 0);
 	}
 } # API ##################################################
 sub blkr
@@ -81,11 +81,11 @@ sub blkr
 	open(my $ifh, '<', "$ipath") || print $Lfh "Cant open $i: $!\n";
 	binmode($ifh);
 	
-	while (read($ifh, $block, SIZE))
+	while (read($ifh, $block, $SIZE))
 	{
 		my $bsha = sha256_hex($block);
-		my $bname = $PATH.'sea/'.$bsha;
-		open(my $fh, '>', "$bname");
+		my $b$NAME = $PATH.'sea/'.$bsha;
+		open(my $fh, '>', "$b$NAME");
 		binmode($fh);
 		print $fh $block;
 		key($i, $bsha);
@@ -96,7 +96,7 @@ sub build
 {
 	my ($i) = @_;
 	my $kpath = $PATH.'key/'.$i;
-	my $dpath = DUMP.'tmp';
+	my $dpath = $DUMP.'tmp';
 
 	open(my $kfh, '<', $kpath);
 	my @set = readline $kfh; chomp @set;
@@ -106,7 +106,7 @@ sub build
 		open(my $tfh, '>>', "$dpath");
 		open(my $ifh, '<', "$ipath");
 		my $block;
-		read($ifh, $block, SIZE);
+		read($ifh, $block, $SIZE);
 		print $tfh $block;
 # DEBUG
 #	my $bsha = sha256_hex($block);
@@ -130,9 +130,9 @@ sub xtrac
 	if ($archive->is_naughty)
 		{ print $Lfh "ALERT xtrac naughty $i"; next; }
 	my @files = $archive->files; print $Lfh @files;
-	$archive->extract(DUMP);
-	XS(DUMP, $PATH) && rmtree(DUMP);
-	mkdir DUMP;
+	$archive->extract($DUMP);
+	XS($DUMP, $PATH) && rmtree($DUMP);
+	mkdir $DUMP;
 	print $Lfh "YAY $i\n"; $YAY++;
 }
 sub regx
@@ -150,30 +150,30 @@ sub get
 {
 	my ($i) = @_;
 	my $ua = uagent();
-	my $response = $ua->get($i, ':content_file'=>"DUMP$i");
+	my $response = $ua->get($i, ':content_file'=>"$DUMP$i");
 	print $Lfh "YAY $i\n"; $YAY++;
 	XS($i) && unlink($i);
 }
 sub arki
 {
 	my ($i) = @_;
-	sleep 1;
+	$SLEEP 1;
 	my $ua = uagent();
-	my $file = DUMP."$i.pdf";
-	my $mfile = DUMP."$i".'_meta.xml';
-	my $url = ARK."$i/$i.pdf";
-	my $murl = ARK."$i/$i".'_meta.xml';	
+	my $file = $DUMP."$i.pdf";
+	my $mfile = $DUMP."$i".'_meta.xml';
+	my $url = $ARK."$i/$i.pdf";
+	my $murl = $ARK."$i/$i".'_meta.xml';	
 	my $resp = $ua->get($url, ':content_file'=>$file); 
 	my $mresp = $ua->get($murl, ':content_file'=>$mfile);
 	if (-f $file) 
 		{ print $Lfh "YAY $i\n"; $YAY++; }
 	else
 	{ 
-		my $eresp = $ua->get(ARK.$i, ':content_file'=>DUMP.'/tmp');
+		my $eresp = $ua->get($ARK.$i, ':content_file'=>$DUMP.'/tmp');
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		my $redo = `grep pdf DUMP.'tmp' | sed 's?</a>.*??' | sed 's/.*>//'`;
+		my $redo = `grep pdf $DUMP.'tmp' | sed 's?</a>.*??' | sed 's/.*>//'`;
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		my $rresp = $ua->get(ARK."$i/$redo", ':content_file'=>$file);
+		my $rresp = $ua->get($ARK."$i/$redo", ':content_file'=>$file);
 		if (-f $file) 
 			{ print $Lfh "YAY $i\n"; $YAY++; }
 		else 
@@ -191,7 +191,7 @@ sub XS
 	my $cur = $PATH."g/g$sha";
 	open(my $fh, '>>', $cur) or die "Meta File Creation FAIL $file";
 	printf $fh "%s\n%s\n%s\n%s\n", 
-		xsname($file),
+		xs$NAME($file),
 		xspath($file),
 		xssize($file),
 		file_mime_encoding($file);
@@ -202,7 +202,7 @@ sub file_digest {
 	$digester->addfile( $file, 'b' );
 	return $digester->hexdigest;
 }
-sub xsname {
+sub xs$NAME {
 	my ($file) = @_;
 	$file =~ s?^.*/??;
 	return $file;
@@ -215,7 +215,7 @@ sub xspath {
 sub file_mime_encoding {
 	my ($file) = @_;
 	my $magic = File::LibMagic->new();
-	my $info = $magic->info_from_filename($file);
+	my $info = $magic->info_from_file$NAME($file);
 	my $des = $info->{description};
 	$des =~ s?[/ ]?.?g;
 	$des =~ s/,/_/g;
@@ -254,28 +254,28 @@ sub api
 	{
 		print $Lfh "FAIL_API $api\n";
 		close $qfh;
-		move(QUE, $PATH.'cemetery/zombie_'.NAME);
+		move($QUE, $PATH.'cemetery/zombie_'.$NAME);
 		return -1;
 	}
 	return 0;	
 }
-sub SUICIDE
+sub $SUICIDE
 {
 	my ($api, $count, $ttl) = @_;
-	unlink SUICIDE;
+	unlink $SUICIDE;
 	printf $Lfh ("FKTHEWORLD %s\n", TIME());
-	tombstone($api, $count, $ttl);
+	$TOMBstone($api, $count, $ttl);
 	exit;
 }
-sub SLEEP
+sub $SLEEP
 {
 	my ($api, $count, $ttl) = @_;
-	open(my $Sfh, '<', SLEEP);
+	open(my $Sfh, '<', $SLEEP);
 	my $timeout = readline $Sfh; chomp $timeout;
-	print $Lfh ("sleep %s %s\n", $timeout, TIME());
-	close $Sfh; unlink SLEEP;
-	tombstone($api, $count, $ttl);
-	sleep $timeout;
+	print $Lfh ("$SLEEP %s %s\n", $timeout, TIME());
+	close $Sfh; unlink $SLEEP;
+	$TOMBstone($api, $count, $ttl);
+	$SLEEP $timeout;
 }
 sub TIME
 {
@@ -286,26 +286,26 @@ sub TIME
 	my $time = $mon.'_'.$day.'_'.$hour;
 	return $time;
 }
-sub name
+sub $NAME
 {
 	my $id = int(rand(999));
-	my $name = $$.'_'.$id;
-	return $name;
+	my $$NAME = $$.'_'.$id;
+	return $$NAME;
 }
-sub tombstone
+sub $TOMBstone
 {
 	my ($api, $count, $ttl) = @_;
 	
-	open(my $LLfh, '<', TOMB);
+	open(my $LLfh, '<', $TOMB);
 	my @llfh = readline $LLfh;
 	my @yay = grep { /^YAY / } @llfh; 
 	my $yay = @yay;
 	my @FACE;
-	$FACE[0] = NAME;	
-	$FACE[1] = ((gmtime() - BIRTH) / 60);
+	$FACE[0] = $NAME;	
+	$FACE[1] = ((gmtime() - $BIRTH) / 60);
 	$FACE[2] = $api . '_' $yay . '_' . $count . '_' . $ttl;
 	
-	open(my $Tfh, '>>', TOMB); 
+	open(my $Tfh, '>>', $TOMB); 
 	printf $Tfh ("%d %s %d %s\n", $YAY, $FACE[0], $FACE[1], $FACE[2]);
 }
 sub key
