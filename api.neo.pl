21f1ea2223a93b6d4eb420b3223099f52541acb7
Prince of Archives <rich.tijerina@gmail.com>
2017-07-24 21:48:58 -0700

    Update api.neo.pl

--- api.neo.pl
+++ api.neo.pl
@@ -6,11 +6,13 @@ sub neo
 	sleep 5;
 	my $ua = uagent();
 	my $url = $NEO.$point;
-	my $resp = $ua->get($url, ':content_file'=>$DUMP.$point); 
-	if (-e $DUMP.$point) 
+	my $file = $DUMP.$point;
+	my $resp = $ua->get($url, ':content_file'=>$file); 
+	if (-e $file) 
 		{ print $Lfh "YAY $point\n"; }
 	else
 		{ print $Lfh "FAIL $point\n"; }
+	XS($file) && unlink($file);
 }
 sub uagent
 {
@@ -20,4 +22,51 @@ sub uagent
 		timeout => 45,
 	);
 	return $s_ua;
+}
+# XS ###########################################################
+sub XS
+{
+	my ($file) = shift;
+	my ($sha) = file_digest($file) or die "couldn't sha $file";
+	File::Copy::copy($file, $PATH."pool/$sha");
+	my $cur = $PATH."g/g$sha";
+	open(my $fh, '>>', $cur) or die "Meta File Creation FAIL $file";
+	printf $fh "%s\n%s\n%s\n%s\n", 
+		xsname($file),
+		xspath($file),
+		xssize($file),
+		file_mime_encoding($file);
+}
+sub file_digest {
+	my ($file) = @_;
+	my $digester = Digest::SHA->new('sha256');
+	$digester->addfile( $file, 'b' );
+	return $digester->hexdigest;
+}
+sub xsname {
+	my ($file) = @_;
+	$file =~ s?^.*/??;
+	return $file;
+}
+sub xspath {
+	my ($file) = @_;
+	$file =~ s?/?_?g;
+	return $file; 
+}
+sub file_mime_encoding {
+	my ($file) = @_;
+	my $magic = File::LibMagic->new();
+	my $info = $magic->info_from_filename($file);
+	my $des = $info->{description};
+	$des =~ s?[/ ]?.?g;
+	$des =~ s/,/_/g;
+	my $md = $info->{mime_type};
+	$md =~ s?[/ ]?.?g;
+	my $enc = sprintf("%s %s %s", $des, $md, $info->{encoding}); 
+	return $enc;
+}
+sub xssize {
+	my ($file) = @_;
+	my $size = -s $file;
+	return $size;
 }
