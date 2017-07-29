bde7c310bff6b93dffb3ed7fa8b28e4e2ad25d41
Prince of Archives <rich.tijerina@gmail.com>
2017-07-28 22:34:27 -0700

    Create MASTER.pl

---
+++ MASTER.pl
@@ -0,0 +1,28 @@
+#!/usr/local/bin/perl
+use Term::ANSIColor;
+
+# 'api/'
+# 'sea/'
+# 'pool/'
+# 'work/'
+# 'cemetery/'
+
+# DEMON_LOG moves from 'work/' to 'cemetery/'
+# When DEMON is disconnected
+
+# FACE is the status
+# tomestone is the outbut
+
+# y_winsize = 144
+# x_winsize = 10
+
+# FORMAT
+####### HUBBUB0 ########### ####### HUBBUB 1 ############
+# $YAY $NAME $AGE           $YAY $NAME $AGE
+# $api $yay $count $ttl     $api $yay $count $ttl
+#
+# $i_time $i                $i_time $i 
+# .                         .
+# .                         .
+# .                         .
+##########################  #############################
