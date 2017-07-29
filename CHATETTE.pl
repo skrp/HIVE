cdfb1281c59c705d18cf3dcf6d55f468d962d63a
Prince of Archives <rich.tijerina@gmail.com>
2017-07-29 00:03:55 -0700

    Update CHATETTE.pl

--- CHATETTE.pl
+++ CHATETTE.pl
@@ -14,8 +14,21 @@ sub fuk_log
 sub key_up
 {
   sleep 60;
-  my ($offset) = @_;
   my $key;
-  read($Kfh, $key, $offset);
+  read($Kfh, $key, $OFFSET);
   return $key;
+}
+sub tarit
+{
+
+}
+sub kripit
+{
+	my ($tarball, $path_server) = @_;
+	my $key = keyup();
+	my $name = dumpname();
+	`openssl enc -aes-256-ecb -in $tarball -out $name -pass:stdin $key`;
+	update_pskey();
+	my $return = `scp $name $path_server`;
+	print $Lfh "$return\n";
 }
