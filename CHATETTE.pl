d4ef3a0582b0a3fd9e35fe229cc74e756aad7428
Prince of Archives <rich.tijerina@gmail.com>
2017-07-28 23:57:16 -0700

    Update CHATETTE.pl

--- CHATETTE.pl
+++ CHATETTE.pl
@@ -1,3 +1,21 @@
 ########################
 # CHATETTE - log updater
 
+sub fuk_log
+{
+	while (1)
+	{
+		my ($Tfh, \@new_items) = @_;
+		printf $Tfh @new_items;
+		`scp $tmp $server:$PATH/work/$tmp`;
+		sleep 60;
+	}
+}
+sub key_up
+{
+  sleep 60;
+  my ($offset) = @_;
+  my $key;
+  read($Kfh, $key, $offset);
+  return $key;
+}
