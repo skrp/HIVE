da844b4a8004b210f7292a825957f509609935b5
Prince of Archives <rich.tijerina@gmail.com>
2017-08-19 03:24:17 -0700

    Update BLKR.c

--- BLKR.c
+++ BLKR.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+////////////////////////////////
+// BLKR - shred file into blocks
+
+SIZE 128000
+int main(int argv, char *argc)
+{
+ char *filepath = argv1;
+ FILE *ifh;
+ char *buf[SIZE]
+   
+ if ((open(ifh, const char *filepath, O_RDONLY)) < 0)
+  printf("FAIL open %p\n", *filepath);
+ 
+ while (read(ifh, void *buf, size_t SIZE) > 0)
+ {
+   sha256(*buf);
+   FILE *bfh;
+   if ((open(bfh, const char *filepath, O_WRONLY)) < 0) 
+      printf("FAIL block open %p\n", *filepath);
+   if ((fprintf(FILE *bfh, const char *buf)) < 0) 
+      printf("FAIL block write %p\n", *filepath);
+   key(*bsha);
+ }
+
+
+sub key
+{
+	my ($i, $bsha) = @_;
+	my $kpath = $PATH.'key/'.$i;
+	open(my $kfh, '>>', "$kpath");
+	print $kfh "$bsha\n";
+}
